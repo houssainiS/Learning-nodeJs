ab0b922c7f448a2b44a34e37363502310280b91f
houssainiS <houssaini.hs@gmail.com>
2024-03-23 15:26:13 +0100

    learned get/post/put/delete

--- server.js
+++ server.js
@@ -2,6 +2,24 @@ const express = require('express');
 
 const app = express();
 
+app.post('/add',()=>{
+    console.log('add works')
+})
+
+app.get('/get',()=>{
+    console.log("getting all data")
+});
+
+app.put( '/update',()=>{ 
+    console.log('updating the database');
+});
+
+app.delete("/delete", () => { 
+    console.log("Deleting from Database");  
+}); 
+
+
+
 app.listen(3000, () => {
     console.log('server work')
-});
+}); // http://127.0.0.1:3000/
